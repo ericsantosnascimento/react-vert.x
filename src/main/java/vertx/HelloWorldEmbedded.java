b07893014dd8b61d05b98af86d7785987cd5396f
Eric Nascimento <ericsantosnascimento@gmail.com>
2017-12-05 00:30:43 +0100

    Hello vert.x

---
+++ src/main/java/vertx/HelloWorldEmbedded.java
@@ -0,0 +1,12 @@
+package vertx;
+
+import io.vertx.core.Vertx;
+
+public class HelloWorldEmbedded {
+
+  public static void main(String[] args) {
+    // Create an HTTP server which simply returns "Hello World!" to each request.
+    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+  }
+
+}
